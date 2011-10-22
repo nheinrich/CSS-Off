5483a24e5384dbcfd4905592d44e1850b769a4fa
Neil Heinrich <neil@pixels-and-bits.com>
2011-10-21 18:29:06 -0700

    javascript clean up for legibility and consistency

--- javascripts/knucklelodeon.js
+++ javascripts/knucklelodeon.js
@@ -8,52 +8,85 @@ $(function(){
 // namespace
 var k = {};
 
+
 // obstacles
 k.obstacles = {
   init: function(){
-    this.element = $("#obstacles")
-    this.links = this.element.find("a")
-    this.links.click(function(){ k.obstacles.show($(this)) })
-    $(this.links[this.random_obstacle()]).click()
+    // store common elements
+    this.$element = $("#obstacles")
+    this.$links = this.$element.find("a")
+
+    // add click event to links
+    this.$links.click(function(){ k.obstacles.show($(this)) })
+
+    // load random obstacle
+    $(this.$links[this.random_obstacle()]).click()
   },
   random_obstacle: function(){
-    return Math.floor(Math.random() * this.links.length)
+    // grabs a random obstacle based on number links
+    return Math.floor(Math.random() * this.$links.length)
   },
   show: function($link){
-    var $obstacle = this.element.find("#obstacle");
-    $obstacle.find("h1").html($link.html());
-    $obstacle.find("h2").html($link.attr("title"));
+    var $obstacle = this.$element.find("#obstacle");
     var image_url = "images/obstacles/";
-    var src = image_url + $link.attr("href").replace("#","") + ".png";
-    $obstacle.find("img").attr("src", src);
-    this.element.find(".selected").removeClass("selected");
+    var name = $link.attr("href").replace("#","")
+    var src = image_url + name + ".png";
+
+    // remove current selection on link list
+    this.$element.find(".selected").removeClass("selected");
+
+    // load new image
+    $obstacle.find("img").fadeOut(function(){
+      // add descriptive class to #obstacle element
+      // note: this can shift the image due to styling so this is done here
+      $obstacle.attr("class","").addClass(name)
+      // alter src
+      $(this).attr("src", src).fadeIn();
+    })
+
+    // populate h1
+    $obstacle.find("h1").fadeOut(function(){
+      $(this).html($link.html()).fadeIn();
+    })
+
+    // populate h2
+    $obstacle.find("h2").fadeOut(function(){
+      $(this).html($link.attr("title")).fadeIn();
+    })
+
+    // set new selection on link list
     $link.closest("li").addClass("selected");
   }
 }
+
 
 // navigation opacity change when not at scrollTop
 k.scroll = {
   init: function(){
     $(document).scroll(function(){
+      var scroll_top = $(this).scrollTop()
+
       // nav
-      if ($(this).scrollTop() > 0) {
+      if (scroll_top > 0) {
         $("body > nav").addClass("scrolling");
       } else {
         $("body > nav").removeClass("scrolling");
       }
+
       // timer
-      if (($(this).scrollTop() > 2250) && k.timer.timing != true) {
+      if (scroll_top > 2250 && k.timer.timing != true) {
         k.timer.init();
       }
     })
   }
 };
 
+
 // timer
 k.timer = {
   init: function(){
-    this.element = $("#timer")
-    this.current_number = parseInt(this.element.html())
+    this.$element = $("#timer")
+    this.current_number = parseInt(this.$element.html())
     this.countdown()
     this.timing = true
   },
@@ -63,11 +96,11 @@ k.timer = {
       if (num.toString().length == 1) {
         num = "0" + num.toString();
       }
-      this.element.html(num);
+      this.$element.html(num);
       this.current_number -= 1
       setTimeout(function(){ k.timer.countdown() }, 1000);
     } else {
-      this.element.html("00");
+      this.$element.html("00");
       $("#register aside span").html("Times Up!<br/>(Just Kidding)")
     }
   }
